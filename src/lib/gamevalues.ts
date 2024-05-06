0799e1fa0aec0f6c9f2ff7a62ad8322ffcbb524a
hyuckkim <monkeyhyuck@gmail.com>
2024-05-06 10:06:24 +0900

    move houes's function to new file

--- src/lib/gamevalues.ts
+++ src/lib/gamevalues.ts
@@ -1,8 +1,7 @@
-import { get, writable } from "svelte/store";
-import { getRes } from "../assets/image";
+import { writable } from "svelte/store";
 import { spring } from "svelte/motion";
-import { addProps, initializeProps, moveDays, newProp } from "./objects/prop";
-import { latestT } from "./values";
+import { initializeProps } from "./objects/prop";
+import { settingHouseProps } from "./objects/house";
 
 export const maxHealth = 3000;
 export const health = writable(maxHealth);
@@ -18,63 +17,10 @@ export function reset() {
   statesEnteredTime.set(0);
   state.set("none");
   initializeProps();
-  addProps(newProp({
-    img: getRes("prop_bed"),
-    source: [64, 0, 32, 64],
-    pos: [80, 80, 48, 96],
-    display: "always",
-    onClick: () => {
-      changeAwakenState(get(latestT))
-      return true;
-    }
-  }));
-  addProps(newProp({
-    img: getRes("prop_house"),
-    source: [89, 49, 205, 289],
-    pos: [110, 110, 180, 260],
-    layer: "roof",
-  }));
-  addFloor();
+  settingHouseProps();
 }
 
-function addFloor() {
-  const posDatas: [x: number, y: number, w: number, h: number][] = [
-    [60, 50, 48, 48],
-    [108, 50, 48, 48],
-    [156, 50, 48, 48],
-    [60, 98, 48, 48],
-    [108, 98, 48, 48],
-    [156, 98, 48, 48],
-    [60, 146, 48, 48],
-    [108, 146, 48, 48],
-    [156, 146, 48, 48],
-    [60, 194, 48, 48],
-    [108, 194, 48, 48],
-    [156, 194, 48, 48],
-  ];
-  posDatas.forEach(p => {
-    addProps(newProp({
-      img: getRes("prop_tile"),
-      source: [192, 0, 32, 32],
-      pos: p,
-      layer: "floor",
-    }));
-  })
-}
 
 export function resetCharacterPos(x: number, y: number) {
   characterPos.set({x, y});
 } 
-
-export function changeAwakenState(time: number) {
-  const current = get(state);
-  lastCharacterPos.set(get(characterPos));
-  if (current === "awake") {
-    moveDays();
-    state.set("sleep");
-    statesEnteredTime.set(time);
-  } else if (current === "sleep") {
-    state.set("awake");
-    statesEnteredTime.set(time);
-  }
-}
---
+++ src/lib/objects/house.ts
@@ -0,0 +1,73 @@
+import { get } from "svelte/store";
+import { getRes } from "../../assets/image";
+import { addProps, dayEnd, dayStarted, newProp } from "./prop";
+import { characterPos, lastCharacterPos, state, statesEnteredTime } from "../gamevalues";
+import { latestT } from "../values";
+import { generatePlant } from "./plant";
+
+export function settingHouseProps() {
+  makeHouse();
+  makeRoof();
+  makeFloor();
+}
+function makeHouse() {
+  addProps(newProp({
+    img: getRes("prop_bed"),
+    source: [64, 0, 32, 64],
+    pos: [80, 80, 48, 96],
+    display: "always",
+    onClick: () => {
+      changeAwakenState(get(latestT));
+      return true;
+    }
+  }));
+}
+function changeAwakenState(time: number) {
+  const current = get(state);
+  lastCharacterPos.set(get(characterPos));
+  if (current === "awake") {
+    state.set("sleep");
+    statesEnteredTime.set(time);
+    dayEnd();
+
+    addProps(generatePlant());
+    addProps(generatePlant());
+    addProps(generatePlant());
+  } else if (current === "sleep") {
+    state.set("awake");
+    statesEnteredTime.set(time);
+    dayStarted();
+  }
+}
+function makeRoof() {
+  addProps(newProp({
+    img: getRes("prop_house"),
+    source: [89, 49, 205, 289],
+    pos: [110, 110, 180, 260],
+    layer: "roof",
+  }));
+}
+function makeFloor() {
+  const posDatas: [x: number, y: number, w: number, h: number][] = [
+    [60, 50, 48, 48],
+    [108, 50, 48, 48],
+    [156, 50, 48, 48],
+    [60, 98, 48, 48],
+    [108, 98, 48, 48],
+    [156, 98, 48, 48],
+    [60, 146, 48, 48],
+    [108, 146, 48, 48],
+    [156, 146, 48, 48],
+    [60, 194, 48, 48],
+    [108, 194, 48, 48],
+    [156, 194, 48, 48],
+  ];
+  posDatas.forEach(p => {
+    addProps(newProp({
+      img: getRes("prop_tile"),
+      source: [192, 0, 32, 32],
+      pos: p,
+      layer: "floor",
+    }));
+  })
+}
