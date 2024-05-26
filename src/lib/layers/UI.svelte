419c16fe7f6d51b027f600a60b09c9a290c9641d
hyuckkim <monkeyhyuck@gmail.com>
2024-05-26 09:16:12 +0900

    move equip's ui to UI.svelte

--- src/lib/layers/UI.svelte
+++ src/lib/layers/UI.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Layer } from "svelte-canvas";
+  import { Layer, type Render } from "svelte-canvas";
   import {
     characterPos,
     health,
@@ -11,9 +11,12 @@
   } from "../gamevalues";
   import { HealthBarExtraHeight } from "../gamevalues";
   import { onMount } from "svelte";
-  import { drawHealthBar } from "./ui";
-  import { getCurrentProps } from "../objects/prop";
-  import { mouseX, mouseY, pos, rClick } from "../values";
+  import { drawHealthBar, drawItemPanel } from "./ui";
+  import { drawPropImg, getCurrentProps } from "../objects/prop";
+  import { mouseX, mouseY, pos, rClick, type Coord } from "../values";
+  import { equips } from "../objects/equip";
+  import { drawSprite } from "./sprite";
+  import { getRes } from "../../assets/image";
 
   const marginX = 20;
   const marginY = 5;
@@ -23,10 +26,9 @@
       $HealthBarExtraHeight = 0;
     }, 0);
   });
-</script>
-<Layer
-  render={(canvas) => {
-    const { context, width, height } = canvas;
+
+  const renderHealthBar: Render = (canvas) => {
+    const { width, height } = canvas;
     const tempHealth =
       $rClick && $state === "awake"
         ? Math.abs($pos.x + $pos.y - ($mouseX + $mouseY))
@@ -54,31 +56,35 @@
           Math.min(m, initializeMaxHealth)
       );
     }
+  }
 
+  const renderAttachedUI: Render = (canvas) => {
     getCurrentProps()
       .filter((p) => p.isAttached($characterPos.x, $characterPos.y))
       .forEach((p) => {
         p.ui(canvas, p.state);
-      });
+    });
+  }
 
-    if ($rClick) {
-      context.save();
-      context.font = `10px Verdana`;
-      context.fillStyle = "#fffa";
-      context.strokeStyle = "#fffa";
-      context.lineWidth = 3;
-      context.beginPath();
-      context.moveTo($pos.x, $pos.y + 30);
-      context.lineTo($mouseX, $mouseY + 30);
-      context.fillText(
-        `${Math.floor(Math.abs($pos.x - $mouseX))} x ${Math.floor(Math.abs($pos.y - $mouseY))}`,
-        $mouseX + 5,
-        $mouseY + 35
-      );
-      context.stroke();
-      context.restore();
-    }
+  const renderDistanceUI: Render = ({ context }) => {
+    context.save();
+    context.font = `10px Verdana`;
+    context.fillStyle = "#fffa";
+    context.strokeStyle = "#fffa";
+    context.lineWidth = 3;
+    context.beginPath();
+    context.moveTo($pos.x, $pos.y + 30);
+    context.lineTo($mouseX, $mouseY + 30);
+    context.fillText(
+      `${Math.floor(Math.abs($pos.x - $mouseX))} x ${Math.floor(Math.abs($pos.y - $mouseY))}`,
+      $mouseX + 5,
+      $mouseY + 35
+    );
+    context.stroke();
+    context.restore();
+  }
 
+  const renderSeedText: Render = ({ context, width, height }) => {
     context.save();
     context.font = `10px Verdana`;
     context.fillStyle = "#fffa";
@@ -88,13 +94,32 @@
       15
     );
     context.restore();
+  }
 
-    if ($resetTry) {
-      context.save();
-      const data = ["#0000", "#0003", "#0006", "#0009", "#000b"];
-      context.fillStyle = data[$resetTry];
-      context.fillRect(0, 0, width, height);
-      context.restore();
+  const renderItemPanel: Render = ({ context, width, height }) => {
+    drawSprite(context, getRes("ui"), [width, height - 114, 10, 36], [116, 333, 5, 18], { x: false, y: false}, {x: 1, y: 0.5});
+    drawItemPanel(context, [width - 66, height - 144, 60, 60]);
+    if ($equips) {
+      const itemPropCoord: Coord = [width - 34, height - 114, $equips.pos[2], $equips.pos[3]];
+      drawPropImg(context, {img: $equips.img, state: {...$equips.state, pos: itemPropCoord}});
     }
+  }
+
+  const renderResetDark: Render = ({ context, width, height }) => {
+    context.save();
+    const data = ["#0000", "#0003", "#0006", "#0009", "#000b"];
+    context.fillStyle = data[$resetTry];
+    context.fillRect(0, 0, width, height);
+    context.restore();
+  }
+</script>
+<Layer
+  render={(canvas) => {
+    renderHealthBar(canvas);
+    renderAttachedUI(canvas);
+    renderSeedText(canvas);
+    renderItemPanel(canvas);
+    if ($rClick) renderDistanceUI(canvas);
+    if ($resetTry) renderResetDark(canvas);
   }}
 />
--- src/lib/layers/equip.svelte
+++ src/lib/layers/equip.svelte
@@ -2,19 +2,15 @@
   import { Layer, type Render } from "svelte-canvas";
   import { characterDir, characterPos } from "../gamevalues";
   import { addCoord } from "../values";
-  import { drawItemPanel } from "./ui";
   import type { Equip } from "../objects/equip";
   import type { Coord } from "../values";
   import { drawPropImg } from "../objects/prop";
 
   export let equip: Equip;
   $: equipPos = [-7, 0, 0, 7][$characterDir];
-  const render: Render = ({ context, width, height }) => {
+  const render: Render = ({ context }) => {
   const propCoord: Coord = [$characterPos.x + equipPos, $characterPos.y, 0, 0];
-  const itemPropCoord: Coord = [width - 34, height - 114, equip.pos[2], equip.pos[3]]
   drawPropImg(context, {img: equip.img, state: {...equip.state, pos: addCoord(equip.pos, propCoord)}});
-  drawItemPanel(context, [width - 64, height - 144, 60, 60]);
-  drawPropImg(context, {img: equip.img, state: {...equip.state, pos: itemPropCoord}});
 }
 </script>
 
